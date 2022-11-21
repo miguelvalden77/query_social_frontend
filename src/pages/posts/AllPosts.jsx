5e122bf486ece3df2edeeba0c5f6eec6917bd463
miguelvalden <miguelvalden77@gmail.com>
2022-11-22 00:14:54 +0100

    deploy

--- src/pages/posts/AllPosts.jsx
+++ src/pages/posts/AllPosts.jsx
@@ -64,8 +64,7 @@ const AllPosts = ()=>{
                         <div className="avatar-container">
                             <img src={avatar} alt="avatar usuario" />
                         </div>
-                        {/* <p>{e.author.username}</p> */
-                        console.log(e.author)}
+                        <p>{e.author.username}</p>
                     </section>
                     <section className="post-section img-post">
                         <img src={e.photo} alt="foto"/>
